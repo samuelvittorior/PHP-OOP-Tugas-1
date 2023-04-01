59dd06e5c7a2e77f36ffa33c12da81fbedbdd524
samuelvittorior <129597319+samuelvittorior@users.noreply.github.com>
2023-04-01 20:07:09 +0700

    Update Tugas1.php

--- Tugas1.php
+++ Tugas1.php
@@ -1,1 +1,35 @@
+<?php
+class Product {
+  protected $name;
+  protected $price;
+  protected $discount;
+  
+  function __construct($name,$price,$discount) {
+    $this->name = $name;
+    $this->price = $price;
+    $this->discount = $discount;
+  }  
+  public function getPrice() {
+    return $this->price;
+  }
+  public function setPrice($new_price) {
+    $this->price = $new_price
+  }
+  public function getName() {
+    return $this->name;
+  }
+  public function setPrice($new_name) {
+    $this->price = $new_name
+  }
+  public function getDiscount() {
+    return $this->discount;
+  }
+  public function setDiscount($new_discount) {
+    $this->price = $new_discount
+  }
+}
 
+
+class CDMusic extends Product
+
+?>  
